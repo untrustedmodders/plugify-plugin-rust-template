8e196ef871373776b11d3d5a8a52b9815949d378
qubka <nikita.ushakov0000@outlook.com>
2025-12-10 23:00:46 +0000

    add example module

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+use plugify::{register_plugin, PlgString};
+
+fn on_plugin_start() {
+    println!("ExamplePlugin: on_plugin_start")
+}
+
+fn on_plugin_update(_dt: f32) {
+    println!("ExamplePlugin: on_plugin_update")
+}
+
+fn on_plugin_end() {
+    println!("ExamplePlugin: on_plugin_end")
+}
+
+register_plugin!(
+    start: on_plugin_start,
+    update: on_plugin_update,
+    end: on_plugin_end
+);
+
+#[unsafe(no_mangle)]
+pub extern "C" fn make_print(i32: count, &PlgString: message) {
+    for _ in 0..count {
+        println!("{}", message);
+    }
+}
